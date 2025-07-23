df33f0199a99403584dccfbab8ea2f4da49b3d44
M.Nabeel Altaf <nabeelaltaf@outlook.com>
2025-07-23 13:26:19 +0500

    latest build on aws

--- quri_backend/db/db.js
+++ quri_backend/db/db.js
@@ -24,7 +24,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'QuriDB#123',
+    password: "QuriDB#123",
     database: process.env.DB_NAME || 'quriapp'
 });
 
