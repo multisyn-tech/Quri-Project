b7d86cf114ccaa4c7fc22106d4c26441ca491f39
MNabeelAltaf <nabeelaltaf@outlook.com>
2025-08-19 17:57:18 +0500

    test,local,prod Mode added

--- quri_frontend/vite.config.js
+++ quri_frontend/vite.config.js
@@ -1,18 +1,46 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': {}
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: 'my-custom-build.js',
+//         chunkFileNames: 'chunks-[name].js',
+//         assetFileNames: 'assets-[name].[ext]',
+//       },
+//     },
+//   },
+// });
+
+
+
+
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'my-custom-build.js',
-        chunkFileNames: 'chunks-[name].js',
-        assetFileNames: 'assets-[name].[ext]',
+export default defineConfig(({ mode }) => {
+  // Load env file based on mode (prod/test/local)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': {}
+    },
+    base: env.VITE_APP_BASE || '/', 
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'my-custom-build.js',
+          chunkFileNames: 'chunks-[name].js',
+          assetFileNames: 'assets-[name].[ext]',
+        },
       },
     },
-  },
+  };
 });
