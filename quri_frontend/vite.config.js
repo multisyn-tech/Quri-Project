75abdea3b34f5a49f38b61852b911a16a02b8ca1
MNabeelAltaf <nabeelaltaf@outlook.com>
2025-08-19 18:35:48 +0500

    latest-build-test

--- quri_frontend/vite.config.js
+++ quri_frontend/vite.config.js
@@ -18,21 +18,19 @@
 // });
 
 
-
-
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on mode (prod/test/local)
-  const env = loadEnv(mode, process.cwd(), '');
+  let base = '/';
+  if (mode === 'test') base = '/test/';
 
   return {
+    base, // 👈 important!
     plugins: [react()],
     define: {
       'process.env': {}
     },
-    base: env.VITE_APP_BASE || '/', 
     build: {
       rollupOptions: {
         output: {
