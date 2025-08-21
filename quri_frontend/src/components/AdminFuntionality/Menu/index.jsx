802ac4b2099f72782b7904bcb2f272c56d3c4322
MNabeelAltaf <nabeelaltaf@outlook.com>
2025-08-21 17:06:51 +0500

    menu screen cleaned

--- quri_frontend/src/components/AdminFuntionality/Menu/index.jsx
+++ quri_frontend/src/components/AdminFuntionality/Menu/index.jsx
@@ -8,6 +8,8 @@ import EditMenu from './editMenu';
 import { fetchMenus, deleteMenu } from '../../../features/menu/menuSlice';
 import { editMenu, fetchMenu } from '../../../features/menu/menuSlice';
 import axios from 'axios';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { ExpandMore } from '@mui/icons-material';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -70,7 +72,11 @@ const Index = () => {
     return status === 'active' ? 'text-green-500' : 'text-red-500';
   };
 
-
+  const groupedMenus = menus?.reduce((acc, menu) => {
+    if (!acc[menu.CategoryName]) acc[menu.CategoryName] = [];
+    acc[menu.CategoryName].push(menu);
+    return acc;
+  }, {});
 
   const handleToggleStatus = async (menu) => {
 
@@ -96,8 +102,9 @@ const Index = () => {
 
   return (
     <>
-      <div className="mt-10 ">
-        <Grid container spacing={3} >
+      <div className="mt-10">
+        <Grid container spacing={3}>
+          {/* Add New Item Button */}
           <Grid item xs={12}>
             <Button
               variant="contained"
@@ -110,11 +117,14 @@ const Index = () => {
             </Button>
           </Grid>
 
+          {/* Loading State */}
           {loading && (
             <Grid item xs={12}>
               <Typography variant="body1" className="text-center dark:text-white">Loading...</Typography>
             </Grid>
           )}
+
+          {/* Error State */}
           {error && (
             <Grid item xs={12}>
               <Typography variant="body1" className="text-center text-red-500 dark:text-red-400">
@@ -123,81 +133,98 @@ const Index = () => {
             </Grid>
           )}
 
-          {Array.isArray(currentPosts) && currentPosts.length > 0 ? (
-            currentPosts.map((menu) => (
-              <Grid item xs={10} sm={6} md={4} lg={3} key={menu.MenuID}>
-                <Paper
-                  className="p-4 dark:bg-gray-800 dark:text-gray-100 h-[100%]"
-                  elevation={3}
-                  style={{ transition: 'background-color 0.3s ease' }}
-                >
-                  <div className="menu-image-container mb-4">
-                    {menu.Image && (
-                      <>
-                        {menu.Image.startsWith('food-uploads/') ? (
-                          <img
-                            src={`${BASE_URL}/${menu.Image}`}
-                            alt={menu.ItemName}
-                            style={menuImageStyle}
-                          />
-                        ) : (
-                          <img
-                            src={menu.Image}
-                            alt={menu.ItemName}
-                            style={menuImageStyle}
-                          />
-                        )}
-                      </>
-                    )}
-                  </div>
-                  <Typography variant="h6" className="dark:text-gray-100">
-                    Name: {menu.ItemName}
-                  </Typography>
-                  <Typography variant="body1" className="dark:text-gray-300">
-                    Price: AED {menu.Price}
-                  </Typography>
-                  <Typography variant="body1" className="dark:text-gray-300">
-                    Ingredients: {menu.ItemDescription}
-                  </Typography>
-                  <Typography variant="body1" className="dark:text-gray-300">
-                    Category: {menu.CategoryName}
-                  </Typography>
-                  <Typography variant="body1" className={getStatusColor(menu.MenuStatus)}>
-                    Menu Status: {menu.MenuStatus}
-                  </Typography>
-                  <div className="flex justify-between mt-4">
-                    <IconButton
-                      color="primary"
-                      size="large"
-                      className="hover:bg-blue-100 dark:hover:bg-blue-900 rounded-full"
-                      onClick={() => openEditMenu(menu.MenuID)}
-                    >
-                      <Edit fontSize="inherit" />
-                    </IconButton>
-                    <IconButton
-                      color="error"
-                      size="large"
-                      className="hover:bg-red-100 dark:hover:bg-red-900 rounded-full"
-                      onClick={() => handleDeleteMenu(menu.MenuID)}
-                    >
-                      <Delete fontSize="inherit" />
-                    </IconButton>
 
+          {/* {Array.isArray(currentPosts) && currentPosts.length > 0 ? (
+          
+            Object.keys(groupedMenus).map((category) => (
+              <Grid item xs={12} key={category}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls={`${category}-content`}
+                    id={`${category}-header`}
+                  >
+                    <Typography variant="h6">{category}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Grid container spacing={2}>
+                  
+                      {currentPosts.filter((menu) => menu.CategoryName === category).map((menu) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={menu.MenuID}>
+                          <Paper
+                            className="p-4 dark:bg-gray-800 dark:text-gray-100 h-[100%]"
+                            elevation={3}
+                            style={{ transition: 'background-color 0.3s ease' }}
+                          >
+                            <div className="menu-image-container mb-4">
+                              {menu.Image && (
+                                <>
+                                  {menu.Image.startsWith('food-uploads/') ? (
+                                    <img
+                                      src={`${BASE_URL}/${menu.Image}`}
+                                      alt={menu.ItemName}
+                                      style={menuImageStyle}
+                                    />
+                                  ) : (
+                                    <img
+                                      src={menu.Image}
+                                      alt={menu.ItemName}
+                                      style={menuImageStyle}
+                                    />
+                                  )}
+                                </>
+                              )}
+                            </div>
+                            <Typography variant="h6" className="dark:text-gray-100">
+                              Name: {menu.ItemName}
+                            </Typography>
+                            <Typography variant="body1" className="dark:text-gray-300">
+                              Price: AED {menu.Price}
+                            </Typography>
+                            <Typography variant="body1" className="dark:text-gray-300">
+                              Ingredients: {menu.ItemDescription}
+                            </Typography>
+                            <Typography variant="body1" className="dark:text-gray-300">
+                              Category: {menu.CategoryName}
+                            </Typography>
+                            <Typography variant="body1" className={getStatusColor(menu.MenuStatus)}>
+                              Menu Status: {menu.MenuStatus}
+                            </Typography>
+                            <div className="flex justify-between mt-4">
+                              <IconButton
+                                color="primary"
+                                size="large"
+                                className="hover:bg-blue-100 dark:hover:bg-blue-900 rounded-full"
+                                onClick={() => openEditMenu(menu.MenuID)}
+                              >
+                                <Edit fontSize="inherit" />
+                              </IconButton>
+                              <IconButton
+                                color="error"
+                                size="large"
+                                className="hover:bg-red-100 dark:hover:bg-red-900 rounded-full"
+                                onClick={() => handleDeleteMenu(menu.MenuID)}
+                              >
+                                <Delete fontSize="inherit" />
+                              </IconButton>
 
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={menu.MenuStatus === "active"}
-                          onChange={() => handleToggleStatus(menu)}
-                          color="success"
-                        />
-                      }
-                      label={menu.MenuStatus === "active" ? "Active" : "Inactive"}
-                    />
-
-
-                  </div>
-                </Paper>
+                              <FormControlLabel
+                                control={
+                                  <Switch
+                                    checked={menu.MenuStatus === "active"}
+                                    onChange={() => handleToggleStatus(menu)}
+                                    color="success"
+                                  />
+                                }
+                                label={menu.MenuStatus === "active" ? "Active" : "Inactive"}
+                              />
+                            </div>
+                          </Paper>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </AccordionDetails>
+                </Accordion>
               </Grid>
             ))
           ) : (
@@ -206,24 +233,138 @@ const Index = () => {
                 No menu available.
               </Typography>
             </Grid>
-          )}
+          )} */}
 
 
-          <Grid item xs={12}>
+          {
+            Array.isArray(currentPosts) && currentPosts.length > 0 ? (
+              Object.keys(groupedMenus).map((category) => {
+                return (
+                  <Grid item xs={12} key={category}>
+                    <Accordion>
+                      <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        aria-controls={`${category}-content`}
+                        id={`${category}-header`}
+                      >
+                        <Typography variant="h6">{category}</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Grid container spacing={2}>
+                          {currentPosts
+                            .filter((menu) => menu.CategoryName === category)
+                            .map((menu) => {
+                              return (
+                                <Grid item xs={12} sm={6} md={4} lg={3} key={menu.MenuID}>
+                                  <Paper
+                                    className="p-4 dark:bg-gray-800 dark:text-gray-100 h-[100%]"
+                                    elevation={3}
+                                    style={{ transition: 'background-color 0.3s ease' }}
+                                  >
+                                    <div className="menu-image-container mb-4">
+                                      {menu.Image && (
+                                        <>
+                                          {menu.Image.startsWith('food-uploads/') ? (
+                                            <img
+                                              src={`${BASE_URL}/${menu.Image}`}
+                                              alt={menu.ItemName}
+                                              style={menuImageStyle}
+                                            />
+                                          ) : (
+                                            <img
+                                              src={menu.Image}
+                                              alt={menu.ItemName}
+                                              style={menuImageStyle}
+                                            />
+                                          )}
+                                        </>
+                                      )}
+                                    </div>
+                                    <Typography variant="h6" className="dark:text-gray-100">
+                                      Name: {menu.ItemName}
+                                    </Typography>
+                                    <Typography variant="body1" className="dark:text-gray-300">
+                                      Price: AED {menu.Price}
+                                    </Typography>
+                                    <Typography variant="body1" className="dark:text-gray-300">
+                                      Ingredients: {menu.ItemDescription}
+                                    </Typography>
+                                    <Typography variant="body1" className="dark:text-gray-300">
+                                      Category: {menu.CategoryName}
+                                    </Typography>
+                                    <Typography
+                                      variant="body1"
+                                      className={getStatusColor(menu.MenuStatus)}
+                                    >
+                                      Menu Status: {menu.MenuStatus}
+                                    </Typography>
+                                    <div className="flex justify-between mt-4">
+                                      <IconButton
+                                        color="primary"
+                                        size="large"
+                                        className="hover:bg-blue-100 dark:hover:bg-blue-900 rounded-full"
+                                        onClick={() => openEditMenu(menu.MenuID)}
+                                      >
+                                        <Edit fontSize="inherit" />
+                                      </IconButton>
+                                      <IconButton
+                                        color="error"
+                                        size="large"
+                                        className="hover:bg-red-100 dark:hover:bg-red-900 rounded-full"
+                                        onClick={() => handleDeleteMenu(menu.MenuID)}
+                                      >
+                                        <Delete fontSize="inherit" />
+                                      </IconButton>
+
+                                      <FormControlLabel
+                                        control={
+                                          <Switch
+                                            checked={menu.MenuStatus === 'active'}
+                                            onChange={() => handleToggleStatus(menu)}
+                                            color="success"
+                                          />
+                                        }
+                                        label={menu.MenuStatus === 'active' ? 'Active' : 'Inactive'}
+                                      />
+                                    </div>
+                                  </Paper>
+                                </Grid>
+                              );
+                            })}
+                        </Grid>
+                      </AccordionDetails>
+                    </Accordion>
+                  </Grid>
+                );
+              })
+            ) : (
+              <Grid item xs={12}>
+                <Typography variant="h6" align="center" className="dark:text-gray-400">
+                  No menu available.
+                </Typography>
+              </Grid>
+            )
+          }
+
+
+
+
+          {/* Pagination */}
+          {/* <Grid item xs={12}>
             <Pagination
               totalPosts={menus.length}
               postsPerPage={postsPerPage}
               setCurrentPage={setCurrentPage}
               currentPage={currentPage}
             />
-          </Grid>
+          </Grid> */}
 
+          {/* Add/Edit Menu Modals */}
           {isAddMenuOpen && <AddMenu onClose={closeAddMenu} />}
           {isEditMenuOpen && <EditMenu onClose={closeEditMenu} menuId={selectedMenuId} />}
         </Grid>
-
-
       </div>
+
     </>
   );
 };
