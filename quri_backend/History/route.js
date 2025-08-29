065a1bdc19b0aeb29994ea0b1865b5be9cfe40db
MNabeelAltaf <nabeelaltaf@outlook.com>
2025-08-29 15:07:30 +0500

    Keep only quri_frontend code in frontend_staging

--- quri_backend/History/route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getCustomerOrderDetails } = require("./controller.js");
-const router = express.Router();
-
-router.get('/:CustomerID', getCustomerOrderDetails);
-
-module.exports = router;
--- quri_backend/SalesReport/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { fetchAllSalesController } = require("./controller.js");
-
-const router = express.Router();
-
-router.get('/sales-report', fetchAllSalesController);
-
-module.exports = router;
